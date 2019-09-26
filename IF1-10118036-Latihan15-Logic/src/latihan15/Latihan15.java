358b7a1ac5cbf2d49dc45d07fdec380607c26906
Adrian C. Senna <55816324+Raykitsuu@users.noreply.github.com>
2019-09-26 19:27:55 +0700

    Add files via upload

---
+++ IF1-10118036-Latihan15-Logic/src/latihan15/Latihan15.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan15;
+
+/**
+ *
+ * @author
+ * NAMA			: Adrian C. Senna
+ * KELAS		: IF-1
+ * NIM			: 10118036
+ * Deskripsi Program	: Program ini berisi program untuk menunjukkan operasi logika.
+ *
+ */
+public class Latihan15 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		boolean a = true;
+		boolean b = false;
+		System.out.println("a && b = " + (a && b));
+		System.out.println("a || b = " + (a || b));
+		System.out.println("!a = " + !a);
+		System.out.println("!(a && b) = " + !(a && b));
+	}
+	
+}
